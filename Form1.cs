cb9b1f53789462ca59faaedd365c5d7df368321d
Kyle Tiley <kylestiley@gmail.com>
2021-09-25 16:22:11 +0200

    skeleton up to 2.4 done

--- Form1.cs
+++ Form1.cs
@@ -28,48 +28,80 @@ namespace GADE_Task_1
     {
         protected int X;
         protected int Y;
-        public enum TileType
-        {
-            Hero,
-            Enemy,
-            Gold,
-            Weapon,
-        }
-        public Tile(int x, int y)
+        public enum TileType {Hero, Enemy, Gold, Weapon}
+        public Tile(int x, int y)   //constructor for the class Tile
         {
             this.X = x;
             this.Y = y;
         }
     }
-    public class Obstacle : Tile
+    public class Obstacle : Tile    //Obstacle class is child of Tile class
     {
-        public Obstacle() : base(1,1)
+        public Obstacle(int x, int y) : base(x,y)   //constructor that calls the base class with the X and Y parameters
         {
-            
+            //set the border values here
         }
     }
     public class EmptyTile : Tile
     {
-        public EmptyTile() : base(1, 1)
+        public EmptyTile(int x, int y) : base(x, y)
         {
-            
+            //set the empty tile values here
         }
     }
 
     //Question 2.2
-    public abstract class Character //must inherit from Tile how to do this??
+    public abstract class Character : Tile
     {
-        int HP;
-        int MaxHP;
-        int Damage;
-        int[] Vision = new int[4];
-        public enum Movement
+        protected int HP;
+        protected int MaxHP;
+        protected int Damage;
+        protected int[] Vision = new int[4];
+        public enum Movement { NoMovement, Up, Down, Left, Right }
+
+        //Question 2.3
+        public Character(int x, int y) : base(x,y)
         {
-            NoMovement,
-            Up,
-            Down,
-            Left,
-            Right,
+            //symbol part still needs to be done?????
         }
+        public virtual void Attack(Character target)    //attacks target and decreases its health
+        {
+
+        }
+        public bool IsDead()    //checks if character is dead
+        {
+            
+        }
+        public virtual bool CheckRange(Character target)    //checks if target is in range of a character
+        {
+            DistanceTo();
+        }
+        private int DistanceTo(target)  //determines absolute distance between a character and its target
+        {
+
+        }
+        public void Move(MovementEnum move) //edits a unit's X and Y values to move it in a direction
+        {
+
+        }
+        public abstract MovementEnum ReturnMove(MovementEnum move = 0)  //returns a direction of movement based on how the character should move
+        {
+
+        }
+        public abstract override ToString() //overrides the Object ToString() method
+        {
+
+        }
+    }
+
+    //Question 2.4
+    abstract class Enemy : Character
+    {
+        Random rnd = new Random();  //random object
+        public Enemy()
+        {
+            
+        }
+        
     }
 }
